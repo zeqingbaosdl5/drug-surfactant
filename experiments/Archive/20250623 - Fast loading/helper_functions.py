72ce4a63490844132b18515d379d08cb785501eb
zeqingbaosdl5 <zeqingbaosdl5@gmail.com>
2025-06-25 14:34:09 -0400

    update

--- experiments/Archive/20250623 - Fast loading/helper_functions.py
+++ experiments/Archive/20250623 - Fast loading/helper_functions.py
@@ -35,7 +35,7 @@ normalize_drug_properties_dict = {
 
 }
 
-torch.set_num_threads(900)
+#torch.set_num_threads(900)
 
 # MPS -> Metal Performance Shaders. Akin to using GPU acceleration
 if torch.backends.mps.is_available():
